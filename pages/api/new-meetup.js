a26e511fb52591464ca2636451a07ecbe86103a7
Muhammad Faris Rafi <farisrafi048@gmail.com>
2024-05-22 11:39:40 +0700

    initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://farisrafi:ahrQArslmYzpinCE@cluster0.bc4um7m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,29 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import axios from "axios";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function NewMeetupPage() {
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetupData) => {
+    const { data } = await axios.post("/api/new-meetup", meetupData);
+
+    console.log(data);
+
+    router.push("/");
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </>
+  );
+}
